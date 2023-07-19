650e7611477d6be01a2cc6577286cb02fc8d65d0
Hadiyo11 <hadyhope@gmail.com>
2023-07-19 08:03:48 -0700

    added comments

---
+++ Javascript/AdvancedJavaScript/Project8/Project8.js
@@ -0,0 +1,15 @@
+//Word Scrambler
+// let word = "Advanced";
+// function randomWord(e) {
+//   let myWord = word.length;
+//   let tempArr;
+//   for (let i = 0; i < myWord; i--) {
+//     console.log(e.value);
+//     let index = Math.floor(Math.random() * e.length);
+//     tempArr += [index];
+//     console.log(tempArr);
+//   }
+// }
+// console.log(randomWord(word));
+
+
