fbd87a8b3da11595723bcb6bd90ccfe3e9f2cb96
Hadiyo11 <hadyhope@gmail.com>
2023-07-24 09:24:39 -0700

    added comments

--- Javascript/AdvancedJavaScript/Chapter8/Chapter8.js
+++ Javascript/AdvancedJavaScript/Chapter8/Chapter8.js
@@ -285,12 +285,12 @@
 // console.log(d.toDateString());
 
 //Practice exercise 8.7
-let future = new Date(2025, 5, 15);
-console.log(future);
-let months = ["January", "February", "March", "April", "May", "June", 
-"July", "August", "September", "October", "November", "December"];
-let day = future.getDate();
-let month = future.getMonth();
-let year = future.getFullYear();
-let myDate = `${months[month-1]} ${day} ${year}`;
-console.log(myDate);
+// let future = new Date(2025, 5, 15);
+// console.log(future);
+// let months = ["January", "February", "March", "April", "May", "June", 
+// "July", "August", "September", "October", "November", "December"];
+// let day = future.getDate();
+// let month = future.getMonth();
+// let year = future.getFullYear();
+// let myDate = `${months[month-1]} ${day} ${year}`;
+// console.log(myDate);
