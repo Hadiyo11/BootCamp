f75202d0ce373289bb92a8fc3589d39807943ae7
Hadiyo11 <hadyhope@gmail.com>
2023-08-06 08:01:53 -0700

    added comments

---
+++ Javascript/AdvancedJavaScript/ExtraProject8/videobooth.js
@@ -0,0 +1,82 @@
+window.onload = function () {
+  let controlLinks = document.querySelectorAll("a.control");
+  for (let i = 0; i < controlLinks.length; i++) {
+    controlLinks[i].onclick = handleControl;
+  }
+  let effectiveLinks = document.querySelectorAll("a.effect");
+  for (let i = 0; i < effectiveLinks.length; i++) {
+    effectiveLinks[i].onclick = setEffect;
+  }
+  let videoLinks = document.querySelectorAll("a.videoSelection");
+  for (let i = 0; i < videoLinks.length; i++) {
+    videoLinks[i].onclick = setVideo;
+  }
+  pushUnpushButtons("video1", []);
+  pushUnpushButtons("normal", []);
+  //helper function to visually depress the video button and normal(no filter) button in the interface
+};
+
+function handleControl(e) {
+  let id = e.target.getAttribute("id"); //want to see who called us by retrieving the id of the element that invoked the handler
+  if (id == "play") {
+    pushUnpushButtons("play", ["pause"]);
+  } else if (id == "pause") {
+    pushUnpushButtons("pause", ["play"]); //if pause button was pushed, play shouldnt be there
+  } else if (id == "loop") {//interface reflect pause 
+    if (isButtonPushed("loop")) {
+      pushUnpushButtons("", ["loop"]);
+    } else {
+      pushUnpushButtons("loop", []);
+    }
+  } else if (id == "mute") {
+    if (isButtonPushed("mute")) {
+      pushUnpushButtons("", ["mute"]);
+    } else {
+      pushUnpushButtons("mute", []);
+    }
+  }
+}
+
+function setEffect(e) {
+  let id = e.target.getAttribute("id");
+  if (id == "normal") {
+    pushUnpushButtons("normal", ["western", "noir", "scifi"]);
+  } else if (id == "western") {
+    pushUnpushButtons("western", ["normal", "noir", "scifi"]);
+  } else if (id == "noir") {
+    pushUnpushButtons("noir", ["normal", "western", "scifi"]);
+  } else if (id == "scifi") {
+    pushUnpushButtons("scifi", ["normal", "western", "noir"]);
+  }
+}
+
+function setVideo(e){
+    let id = e.target.getAttribute("id");
+    if(id == 'video1'){
+        pushUnpushButtons("video1", ["video2"]);
+    }else if(id== "video2"){
+        pushUnpushButtons("video2", ['video1'])
+    }
+}
+
+function pushUnpushButtons(idToPush, idarrayToUnpush) {
+    if (idToPush != "") {
+    let anchor = document.getElementById(idToPush);//grab anchor
+    let theClass = anchor.getattribute("class");//get class attribute
+    if (!theClass.indexOf("selected") >= 0) {
+    theClass = theClass + " selected";
+    anchor.setattribute("class", theClass);
+    let newImage = "url(images/" + idToPush + "pressed.png)";
+    anchor.style.backgroundImage = newImage;
+    }
+    }
+    for (var i = 0; i < idarrayToUnpush.length; i++) {
+    anchor = document.getElementById(idarrayToUnpush[i]);
+    theClass = anchor.getattribute("class");
+    if (theClass.indexOf("selected") >= 0) {
+    theClass = theClass.replace("selected", "");
+    anchor.setattribute("class", theClass);
+    anchor.style.backgroundImage = "";
+    }
+    }
+}   
--- Javascript/AdvancedJavaScript/ExtraProject8/webvilletv.js
+++ Javascript/AdvancedJavaScript/ExtraProject8/webvilletv.js
@@ -1,28 +1,37 @@
-let position = 0;//keep track of which video we are playing
+let position = 0; //keep track of which video we are playing
 let playlist; //hold video playlist array
 let video; //hold a ref to the video element
 
-window.onload = function (){
-    playlist = ["video/preroll.mp4",
-"video/areyoupopular.mp4",
-"video/destinationearth.mp4"
-];
+window.onload = function () {
+  playlist = ["video/preroll", "video/areyoupopular", "video/destinationearth"];
+  video = document.getElementById("video");
+  video.addEventListener("ended", nextVideo, false); //nextVideo is function called when the event happens
+  //add eventlistener for the video ended event
+  video.src = playlist[position] + getFormatExtension();
+  video.load();
+  video.play();
+  alert("Playing " + video.currentSrc);
+};
 
-video = document.getElementById("video");
-video.addEventListener('ended', nextVideo, false)//nextVideo is function called when the event happens
-//add eventlistener for the video ended event
-video.src = playlist[position];
-video.load();
-video.play();
+function nextVideo() {
+  position++;
+  if (position >= playlist.length) {
+    position = 0;
+    //if we hit the end of the list, we loop around by setting position to 0
+  }
+  video.src = playlist[position] + getFormatExtension(); //set the source of the player to thenext video
+  video.load();
+  video.play();
+  alert("Playing " + video.currentSrc);
 }
 
-function nextVideo(){
-    position++;
-    if(position>=playlist.length){
-        position=0;
-//if we hit the end of the list, we loop around by setting position to 0
-    }
-    video.src = playlist[position];//set the source of the player to thenext video
-    video.load();
-    video.play();
+//canPlayType method to check is client can play a video formatt
+function getFormatExtension() {
+  if (video.canPlayType("video/mp4") != "") {
+    return ".mp4";
+  } else if (video.canPlayType("video/webm") != "") {
+    return ".webm";
+  } else if (video.canPlayType("video/ogg") != "") {
+    return ".ogv";
+  }
 }
