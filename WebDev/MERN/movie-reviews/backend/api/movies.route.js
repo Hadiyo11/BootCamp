c10c541ea9224bb789186ef3eaef2c266396ee62
Hadiyo11 <hadyhope@gmail.com>
2023-10-14 10:01:29 -0700

    new work

--- WebDev/MERN/movie-reviews/backend/api/movies.route.js
+++ WebDev/MERN/movie-reviews/backend/api/movies.route.js
@@ -1,7 +1,6 @@
 import express from "express";
 import MoviesController from './movies.controller.js';
-import ReviewsController from './reviews.controller.js'
-
+import ReviewsController from './reviews.controller.js';
 
 const router = express.Router(); // get access to express router
 
@@ -11,5 +10,7 @@ router
 .post(ReviewsController.apiPostReview)
 .put(ReviewsController.apiUpdateReview)
 .delete(ReviewsController.apiDeleteReview)
+router.route("/id/:id").get(MoviesController.apiGetMovieById)
+router.route("/ratings").get(MoviesController.apiGetRatings)
 
 export default router;
