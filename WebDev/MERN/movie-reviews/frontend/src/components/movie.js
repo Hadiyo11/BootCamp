1b879a38217d4b4003e06718935f2bfa8c6d57ed
Hadiyo11 <hadyhope@gmail.com>
2023-10-23 11:07:39 -0700

    new work

--- WebDev/MERN/movie-reviews/frontend/src/components/movie.js
+++ WebDev/MERN/movie-reviews/frontend/src/components/movie.js
@@ -1,5 +1,16 @@
+import React, { useState, useEffect } from "react";
 import MovieDataService from "../services/movies";
 import { Link } from "react-router-dom";
+import moment from "moment";
+
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Media from "react-bootstrap/Media";
+
 const Movie = (props) => {
   const [movie, setMovie] = useState({
     id: null,
@@ -7,6 +18,7 @@ const Movie = (props) => {
     rated: "",
     reviews: [],
   });
+
   const getMovie = (id) => {
     MovieDataService.get(id)
       .then((response) => {
@@ -17,10 +29,89 @@ const Movie = (props) => {
         console.log(e);
       });
   };
+
   useEffect(() => {
     getMovie(props.match.params.id);
   }, [props.match.params.id]);
-  return <div></div>;
+
+  const deleteReview = (reviewId, index) => {
+    MovieDataService.deleteReview(reviewId, props.user.id)
+      .then((response) => {
+        setMovie((prevState) => {
+          prevState.reviews.splice(index, 1);
+          return {
+            ...prevState,
+          };      
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <Image src={movie.poster + "/100px250"} fluid />
+          </Col>
+          <Col>
+            <Card>
+              <Card.Header as="h5">{movie.title}</Card.Header>
+              <Card.Body>
+                <Card.Text>{movie.plot}</Card.Text>
+                {props.user && (
+                  <Link to={"/movies/" + props.match.params.id + "/review"}>
+                    Add Review
+                  </Link>
+                )}
+              </Card.Body>
+            </Card>
+            <br></br>
+            <h2>Reviews</h2>
+            <br></br>
+            {movie.reviews.map((review, index) => {
+              return (
+                <Media key={index}>
+                  <Media.Body>
+                    <h5>
+                      {review.name + " reviewed on "}{" "}
+                      {moment(review.date).format("Do MMMM YYYY")}
+                    </h5>
+                    <p>{review.review}</p>
+                    {props.user && props.user.id === review.user_id && (
+                      <Row>
+                        <Col>
+                          <Link
+                            to={{
+                              pathname:
+                                "/movies/" + props.match.params.id + "/review",
+                              state: { currentReview: review },
+                            }}
+                          >
+                            Edit
+                          </Link>
+                        </Col>
+                        <Col>
+                          <Button
+                            variant="link"
+                            onClick={() => deleteReview(review._id, index)}
+                          >
+                            Delete
+                          </Button>
+                        </Col>
+                      </Row>
+                    )}
+                  </Media.Body>
+                </Media>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Movie;
